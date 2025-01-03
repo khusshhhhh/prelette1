ca2ae9f7ed5c405f25065d0a4c55fcc508230f49
khusshhhhh <workforkhush8@gmail.com>
2025-01-04 10:06:09 +1030

    I AM GROOT15

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $username = "khush";
 $password = "Khush@3160"; // Use your database password
 $dbname = "prelette_customer"; // Replace with your database name
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
