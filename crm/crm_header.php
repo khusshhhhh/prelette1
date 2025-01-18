db60b6cfdcf5e05fba495fa81a27d18e604f618a
khusshhhhh <workforkhush8@gmail.com>
2025-01-18 15:06:17 +1030

    Khush Patel94

--- crm/crm_header.php
+++ crm/crm_header.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['username'])) {
 </head>
 
 <body>
-    <nav class="navbar navbar-dark bg-dark navbar-expand-lg py-3">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg py-4">
         <div class="container">
             <a class="navbar-brand btn btn-outline-primary text-white" href="crm_dashboard.php">Dashboard</a>
             <a class="navbar-brand btn btn-outline-primary text-white"
